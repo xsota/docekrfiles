4cda4957380c69a6f500ee3f7070a335814e3c24
sota <git@xsota.com>
2017-06-08 10:45:01 +0900

    blueprint-gen エラー時にwatchがコケてたのでこけないようにgulp-plumberを追加

--- blueprint-gen/gulpfile.js
+++ blueprint-gen/gulpfile.js
@@ -1,12 +1,19 @@
 var gulp = require('gulp');
 var aglio = require('gulp-aglio');
 var watch = require('gulp-watch');
+var plumber = require('gulp-plumber');
 
 var src = 'md/**/'
 var dest = 'public'
 
 gulp.task('publish', function () {
     gulp.src(`${src}*.md`)
+	.pipe(plumber({
+	    errorHandler: function(error) {
+		console.error(error.message);
+		this.emit('end');
+	    }
+	}))
         .pipe(aglio({ template: 'default' }))
         .pipe(gulp.dest(dest));
 });
